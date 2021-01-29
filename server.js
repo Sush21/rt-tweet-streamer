34a0dada66cb36ef470d295e5fca543da1c6344d
fayceltouili <fayceltouili@gmail.com>
2021-01-28 23:45:14 -0800

    added Twitter credentials to .env

--- server.js
+++ server.js
@@ -1,29 +1,42 @@
+require('dotenv').config();
 const express = require('express');
 const Twitter = require('twitter');
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+const {
+  CONSUMER_KEY,
+  CONSUMER_SECRET,
+  ACCESS_TOKEN_KEY,
+  ACCESS_TOKEN_SECRET
+} = process.env;
+
 
 const client = new Twitter({
-  consumer_key: '',
-  consumer_secret: '',
-  access_token_key: '',
-  access_token_secret: ''
+  consumer_key: CONSUMER_KEY,
+  consumer_secret: CONSUMER_SECRET,
+  access_token_key: ACCESS_TOKEN_KEY,
+  access_token_secret: ACCESS_TOKEN_SECRET
 });
- 
+
+
 const defaults = {
   screen_name: 'realDonaldTrump',
   tweet_mode: 'extended',
   count: 20,
 };
 
-const app = express();
 
 app.route('/:handle')
-  .get(function(req, res) {
+  .get((req, res) => {
     const params = {
       ...defaults,
       max_id: req.query.max_id,
       screen_name: req.params.handle,
     };
-    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+    client.get('statuses/user_timeline', params, (error, tweets, response) => {
       if (!error) {
         res.json(tweets);
       } else {
@@ -32,7 +45,6 @@ app.route('/:handle')
     });
   });
 
-app.listen(3000, function(error) {
-  console.log('Trump listening on port 3000');
+app.listen(3001, error => {
+  console.log('Twitter streamer 🚀 is listening on port 3001');
 });
-
