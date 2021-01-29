90480ffb98093165d8f98bb073185579e8be318c
fayceltouili <fayceltouili@gmail.com>
2021-01-28 23:53:09 -0800

    created Navbar component and added Semantic UI

---
+++ client/src/Navbar.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { handles } from './AppConfig';
+import './Header.css';
+
+const Navbar = () => {
+
+  return (
+    <div >
+      <div></div>
+      <div className="ui three top attached buttons">
+        {handles.map(item =>
+          <button className="button-outline ui twitter button"
+            key={item}
+            onClick={() => setHandle(item)}>
+            @{item.toUpperCase()}
+          </button>)}
+      </div>
+    </div>
+  )
+}
+
+export default Navbar;
